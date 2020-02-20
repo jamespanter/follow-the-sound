6e52f7c3a4b48f8deb7f610aa9801799271c04c6
James Panter <James.Panter@Nology.local>
2020-02-20 17:29:58 +0000

    Added sound

--- fts.js
+++ fts.js
@@ -7,3 +7,18 @@ const setTargetObjectLocation = () => {
 
     console.log(x, y)
 }
+
+window.addEventListener('mousemove', () => {
+    console.log('Cursor x: ' + event.pageX, 'y: ' + event.pageY)
+    var targetObjectPosition = targetObject.getBoundingClientRect();
+
+    console.log('Object x: ' + targetObjectPosition.x, 'y: ' + targetObjectPosition.y)
+    let distance = event.pageX - targetObjectPosition.x;
+    console.log(distance)
+    const audio = new Audio('sound.wav');
+    audio.play();
+})
+
+const win = () => {
+    alert('YOU FOUND THE SOUND')
+}
